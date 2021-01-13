30caddf2cf1adc2e9aa010d69c04e9fd2e036939
Ammar150355 <77393388+Ammar150355@users.noreply.github.com>
2021-01-13 19:37:15 +0500

    Ammar

---
+++ app.js
@@ -0,0 +1,508 @@
+Chapter#01
+
+alert("Good morning")
+
+alert("Error!Please enter a valid password")
+
+alert("Welcome to Js land..\n Happy cooding ")
+
+alert("Welcome to JS land")
+
+alert("Happy cooding")
+
+// alert("Hello i can run JS through my web browsers console")
+
+// Chapter#02
+
+// var userName;
+
+// var myName = ("Ammar")
+
+// var message =("Hello world");
+// alert(message)
+
+// var name = ("John");
+// var age = (15);
+// var course = ("Certified mobile appilication development");
+
+// alert(name);
+// alert(age);
+// alert(course);
+// var food = ("pizza\n pizz\n piz\n pi\n p");
+// alert(food)
+
+
+// var email =(" example@example.com");
+// alert("My email address is email address is"+" "+ email)
+
+// var Book = ("A smart way to learn javasript");
+// alert("I am trying to learn from the book"+" "+ Book)
+
+// var a = ("Yah! I can wirte HTML content through javascript");
+// document.write(a)
+
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// Chapter#03
+
+
+
+
+// var age = (15);
+// alert("I am"+" "+age +" "+" years old")
+
+// var visited = (14);
+// alert("You have visited"+" "+visited+" "+"times")
+
+// var birthYear = (2000);
+// alert("My birth year is"+" "+birthYear+" "+"\n Data type of declared number is variable")
+
+// var visitorsName = ("John");
+// var productTitle = ("T_shirts");
+// var quantity = (5);
+// document.write(visitorsName+" "+"ordered"+" "+ " "+quantity+" " + productTitle+" "+ "on XYZ clothing")
+
+// chapter#04
+
+
+// var ammar,numb1,$class;
+
+// var legal, ilegal20,formula,name,age;
+
+// var @name,!age,*value,+class,_color;
+
+// var head = ("<h1>Rules for naming JS variables</h1>")
+
+// var variable1 = (" Variables must begin with a"+" "+"letter"+" "+"$ <br />" )
+// var variable2= ("Variable names can only contain" +" number"+" "+"$"+" "+ "$my"+" "+ " and" +" "+"1st-variable<br/>" );
+// var variable3 = ("Variable names are case+"+" "+ "sensitive <br />");
+// var variables4 = ("Variable names should not be JS"+" "+"keywords");
+// document.write(head, variable1, variable2, variable3 , variables4);
+
+
+// Chapter#05
+
+
+// var numb1 = (3);
+// var numb2 = (5);
+// var numb3 = (numb1+numb2)
+// document.write("The sum of 3+5 is "+" "+ numb3)
+
+
+// var numb1 = (3);
+// var numb2 = (5);
+// var numb3 = (5-3);
+// document.write("The subtraction of 5-3 is"+" "+numb3)
+
+
+
+// var numb1 = (3);
+// var numb2 =(5);
+// var numb3 = (5*3);
+// document.write("The multiplication of 5*3 is"+" "+numb3)
+
+// var numb1 = (5);
+// var numb2 = (3);
+// var numb3 = (numb1|numb2);
+// document.write("The division of 5|3 is"+" "+numb3)
+
+
+// var numb1 = (5);
+// var numb2 = (3);
+// var numb3 = (5 % 3);
+// document.write("The modulus of 5%3 is"+" "+ numb3)
+
+
+// var vrb = ("Value after variable declaration is:");
+// var inetail  = (6);
+// var b = ++inetial 
+// var add = (b+7);
+// var sub = (add--)
+
+
+// console.log(vrb)
+
+
+// var ticket = (6000);
+// var price5 = (6000*5);
+// var totalprice = (ticket*price5);
+// document.write("Total cost to buy 5 tickets to a movie price is"+" "+totalprice+" "+"PKR")
+
+
+// var table = ("<h1>Table of 2</h1>");
+// document.write("2x1=2<br />2x2=4<br />2x3=6<br />2x4=8<br />2x5=10<br />2x6=12<br />2x7=14<br />2x8=16<br />2x9=18<br />2x10=20<br />");
+ 
+
+// var C = (25);
+// var F = (77);
+// var
+// var ( (f-32)*5|9));
+   
+
+// var price1stditem= 600
+// var price2nditem = (100);
+// var orderquantity1 = (3);
+// var orderquantity2 = (7);
+// var shippingcharges = (100);
+
+// var Price1stitem = (650 );
+// document.write("The price item of 1 is"+" "+Price1stitem);
+// document.write("The price item of 2 is" "+)
+
+
+
+// Chapter No 06 
+// var name = prompt("Enter Your Good Name")
+// alert("Good Morning Mr"  + name)
+
+
+
+// Chapter No 06 To 09 Question No 05
+
+// var Table = prompt("Enter Number Which Table You Want To Print")
+
+
+//      for (i=1; i<=10; i++){
+//         document.write(i*2,"<br>")
+
+//   }
+
+
+
+
+
+// Chapter No 9 
+// var city = prompt("Enter City")
+// alert("Welcome To" + " " + city + " " + "City")
+
+
+
+
+// Chapter No 9 To 11 Question No 02
+// var input  = prompt("Enter Your Gender")
+// if (input = "male"){
+//     alert("Good Morning Mr")
+// }
+// else if ( input = "female"){
+//         alert("Good Morning Miss")
+//         }
+// else {
+//     alert("Your are not Human")
+// }
+
+
+
+// Chapter No 9 
+// var feul = prompt("Enter Your Remain Feul")
+// if (feul<=0.25){
+//     alert("Plz ReFill the Feul in Your Car")
+// }
+// else {
+//     alert("You can Servive")
+// }
+
+
+
+// Chapter No 9 
+// var eng = +prompt("Enter Your English Marks")
+// var computer = +prompt("Enter Your English compter")
+// var syclogy = +prompt("Enter Your English syclogy")
+// var total_marks = 300;
+// var obtain = eng+computer+syclogy;
+// var percentage = eng+computer+syclogy/300*100;
+
+// document.write("Total Marks:", total_marks, "<br>")
+// document.write("Your Obtain Marks:",obtain , "<br>" )
+// document.write("Your Percentage:", percentage)
+
+
+
+
+// Chapter No 9 
+// var guess = prompt("Guess the Number") 
+// var snum = "4";
+// if (guess === snum){
+//     alert("Bingo! Correct Answer")
+// }
+// else {
+//     alert("Wrong Answer Try Again")
+// }
+
+
+
+
+// Chapter No 9 
+// var weather = prompt("Enter Your City Weather")
+// if (weather <= 10){
+//     alert("“OMG! Today’s weather is so Cool.")
+// }
+// else if ( weather <=20){
+//     alert("Today’s Weather is cool.")
+// }
+// else if ( weather <=30){
+//     alert("The Weather today is Normal.")
+// }
+// else if ( weather <=40){
+//     alert("“It is too hot outside.")
+// }
+// else {
+//     alert("May Allah Give You Protect")
+// }
+
+
+
+
+
+    
+// Chapter No 9 
+// var num1 = +prompt("Enter First Number")
+// var num2 = +prompt("Enter Second Number")
+// var operator = prompt("Enter Operator")
+// var add  = num1+num2;
+// var sub  = num1-num2;
+// var multi  = num1*num2;
+// var divid  = num1/num2;
+
+// if (operator=== "+"){
+//     document.write(add)
+// }
+// else if ( operator=== "-"){
+//     document.write(sub)
+// }
+// else if ( operator=== "*"){
+//     document.write(sub)
+// }
+// else if ( operator=== "-"){
+//     document.write(sub)
+// }
+
+
+
+
+
+
+    
+
+// Chapter No 12 To 13 
+// var num1 = prompt("Enter First Number")
+// var num2 = prompt("Enter Second Number")
+// if (num1>=num2){
+//     alert(num1)
+// }
+// else if (num2>=num1){
+//     alert(num2)
+// }
+// else {
+//     alert("Both are equal")
+// }
+
+
+
+// Chapter No 12 To 13 Question No 3
+// var num = prompt("Enter First Number")
+//  if (num<=1000000){
+//      alert("Your Number Is Positve")
+//  }
+//  else if (num== -1000000){
+//      alert("Your Number Is Negative")
+//  }
+//  else {
+//      alert("Your Number Is Zero")
+//  }
+
+
+
+// Chapter No 12 To 13 
+// var enterPass = prompt("Enter Your Password")
+// var pass = "Umair_Solangi"
+// if (enterPass===pass){
+//     alert("Welcome To Our Software Network")
+// }
+// else{
+//     alert("Plz Enter Valid Password")
+// }
+
+
+
+// Chapter No 12 To 13
+// var hour = 13;
+// if (hour <18) {
+// greeting = alert("Good day");
+// }
+// else {
+// greeting = alert("Good evening");
+// }
+
+
+
+
+// Chapter No 12 To 13 
+// var time = prompt("Enter Time In 24 Hour Formet")
+// if (time==13){
+//     alert("You Entred At 1:00 PM")
+// }
+// else if (time==14){
+//     alert("You Entred At 2:00 PM ")
+// }
+// else if (time==15){
+//     alert("You Entred At 3:00 PM")
+// }
+//  else if  (time==16){
+//     alert("You Entred At 4:00 PM")
+// }
+//  else if  (time==17){
+//     alert("You Entred At 5:00 PM")
+// }
+//  else if  (time==18){
+//     alert("You Entred At 6:00 PM")
+// }
+//  else if  (time==19){
+//     alert("You Entred At 7:00 PM")
+// }
+//  else if  (time==20){
+//     alert("You Entred At 8:00 PM ")
+// }
+//  else if  (time==21){
+//     alert("You Entred At 9:00 PM")
+// }
+//  else if  (time==22){
+//     alert("You Entred At 10:00 PM")
+// }
+//  else if  (time==23){
+//     alert("You Entred At 11:00 PM")
+// }
+//  else if  (time=24){
+//     alert("You Entred At 12:00 PM")
+// }
+// else{
+//     alert("You Entred At Wrong Time")
+// }
+
+
+
+
+
+// Chapter No 14 To 16 
+// var student_name;
+
+
+
+// Chapter No 14 To 16 
+// var degree = ["Matric", "FSc", "B Tech", "Bachlor", "Master",  "<br>"]
+// document.write(degree )
+
+
+
+
+
+// Chapter No 17 To 20 
+// var (i=0; i<=100;i++){
+
+// }
+
+// Chapter No 17 To 20 
+// for (i=0; i<=10; i++){
+//     document.write(i,"<br>")
+// }
+
+
+
+// Chapter No 17 To 20 
+// var table = prompt("Enter Which Multiplication Table You Want To Print")
+// function table2(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*2,"<br>")
+//     }
+// }
+// function table3(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*3,"<br>")
+//     }
+// }
+// function table4(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*4,"<br>")
+//     }
+// }
+// function table5(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*5,"<br>")
+//     }
+// }
+// function table6(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*6,"<br>")
+//     }
+// }
+// function table7(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*7,"<br>")
+//     }
+// }
+// function table8(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*8,"<br>")
+//     }
+// }
+// function table9(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*9,"<br>")
+//     }
+// }
+// function table10(){
+//     for (i=1; i<=10; i++){
+//         document.write(i*10,"<br>")
+//     }
+// }
+// if (table==2){
+//     table2()
+// }
+// else if (table==3){
+//     table3()
+// }
+// else if (table==4){
+//     table4()
+// }
+// else if (table==5){
+//     table5()
+// }
+// else if (table==6){
+//     table6()
+// }
+// else if (table==7){
+//     table7()
+// }
+// else if (table==8){
+//     table8()
+// }
+// else if (table==9){
+//     table9()
+// }
+// else if (table==10){
+//     table10()
+// }
+
+
+
+
+// Chapter No 17 To 20 Question No 6
+// for (i=0; i<=10; i++){
+//     document.write(i)
+// }
+
+
+
+// Chapter No 17 To 20 Question No 7
+// var search_product = prompt("What You Want To Buy")
+// var  backery = ["apple", "mango", "banana", "gava"]
+// if (search_product=== backery){
+//     alert(search_product +"Is Avilable")
+// }
+// else{
+//     alert(search_product +"Is Not Avilable")
+// }
+
+
+
+
+
